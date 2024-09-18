496d49788101964dfaf8e337a493502060bd715c
Maynor 125 <maynoldemar@gmail.com>
2024-09-17 21:55:33 -0600

    corrigiendo footer

--- vite-project/src/components/global/Footer.tsx
+++ vite-project/src/components/global/Footer.tsx
@@ -1,29 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Facebook,Twitter,Instagram,Youtube  } from "lucide-react"
+import React from "react";
+import { Link } from "react-router-dom";
+import { Facebook, Twitter, Instagram, Youtube } from "lucide-react";
+import { Separator } from "../ui/separator";
 
 const Footer = () => {
   return (
     <footer className=" md:py-6 w-full bottom-0">
-      <div className="container max-w-7xl flex flex-col md:flex-row items-center justify-between gap-4 text-sm">
-        <p className="text-muted-foreground">&copy; 2024 Dev Legion. All rights reserved.</p>
+            <Separator className="my-5"/>
+      <div className=" flex flex-col md:flex-row items-center justify-between text-sm">
+        <p className="text-muted-foreground">
+          &copy; 2024 Dev Legion. All rights reserved.
+        </p>
         <nav className="flex gap-4 md:gap-6">
-          <Link to="#" >
-            <Facebook className="icon transition duration-200 ease-in-out hover:text-secondary"/>
-          </Link>
-          <Link to="#" >
-            <Twitter className="icon transition duration-200 ease-in-out hover:text-secondary"/>
+          <Link to="#">
+            <Facebook className="icon transition duration-200 ease-in-out hover:text-secondary" />
           </Link>
           <Link to="#">
-           <Instagram className="icon transition duration-200 ease-in-out hover:text-secondary"/>
+            <Twitter className="icon transition duration-200 ease-in-out hover:text-secondary" />
           </Link>
-          <Link to="#" >
-           <Youtube className="icon transition duration-200 ease-in-out hover:text-secondary"/>
+          <Link to="#">
+            <Instagram className="icon transition duration-200 ease-in-out hover:text-secondary" />
+          </Link>
+          <Link to="#">
+            <Youtube className="icon transition duration-200 ease-in-out hover:text-secondary" />
           </Link>
         </nav>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
