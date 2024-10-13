ce497f9bc0fbb677db1edf4346b031dfc883a598
Maynor 125 <maynoldemar@gmail.com>
2024-10-12 22:32:23 -0600

    agregando menu al perfil

--- vite-project/src/components/global/Navbar.tsx
+++ vite-project/src/components/global/Navbar.tsx
@@ -10,6 +10,14 @@ import Logo from '../../../public/logo.png'
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Badge } from "../ui/badge";
 import profile from "/profile.jpg";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Navbar: React.FC<NavbarProps> = ({ isLoggedIn }) => {
   //Constante que usare para saber en que ruta estoy
@@ -112,10 +120,28 @@ const Navbar: React.FC<NavbarProps> = ({ isLoggedIn }) => {
                     </Badge>
                     <span className="sr-only">Notificaciones</span>
                   </Button>
+                  <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="relative h-10 w-10 rounded-full">
                   <Avatar className="h-10 w-10">
                     <AvatarImage src={profile} alt="@johndoe" />
                     <AvatarFallback>SJ</AvatarFallback>
                   </Avatar>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56" align="end" forceMount>
+                <DropdownMenuLabel className="font-normal">
+                  <div className="flex flex-col space-y-1">
+                    <p className="text-sm font-medium leading-none">Sarah Johnson</p>
+                    <p className="text-xs leading-none text-muted-foreground">Sara.doe@example.com</p>
+                  </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Perfil</DropdownMenuItem>
+                <DropdownMenuItem>Configuración</DropdownMenuItem>
+                <DropdownMenuItem>Cerrar sesión</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         )
       }
